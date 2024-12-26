ad20b874567720305134ac8246bed3d8b77889d6
stefano.bozzoni <stefano.bozzoni@alten.it>
2024-12-26 12:46:13 +0100

    implemented the class App

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 This python program helps to create git branches from task number and description.
 It uses tkInter as Graphical user interface and is designed to work with git and it can be used togheter with a chrome extension to get automatically task names and numbers from service now.
 It can be easily adapted to work with other dev ops tools since it does grab data directly from the web page of the task.
+
+#to start the application  
+in the command terminal issue that commands  
+
+pip install pyperclip  
+python main.py  
+  
+before you start the program be sure to modify the first two line of the main.py file:  
+BETTING_FOLDER = <the folder to the betting app that contains the git repository>  
+USERNAME = <your user name>  
+  
+once the program starts you can enter the values of this edit fields:  
+SN number is the task number like BUGT0012833 or STRY0012003  
+the description is a bried description of the task  
+then press "create branch name" button to create the branch name  
+press create branch to create the branch in git and checkout it  
+  
+the "get from clipboard" button can be used in conjuction with the chrome extension so to populate the fields automatically if before you have used the chrome extension to copy the data to the clipboard.  
+  
+See further instructions.  
--- main.py
+++ main.py
@@ -7,151 +7,159 @@ import subprocess
 import pyperclip
 
 BETTING_FOLDER = "C:\dev\scommesse"
+USERNAME = "BOZZONI"
 
-def normalize_text(text):
-    text = unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("ASCII")
-    text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
-    text = re.sub(r"\s+", "-", text)
-    return text
+class App:
+    def __init__(self, root):
+        self.root = root
+        root.title("Sisal Git branch creator")
 
-def check_no_uncommitted_changes():
-    try:
-        # Run 'git status --porcelain' to check for changes
-        result = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True)
+        self.frame = tk.Frame(self.root, padx=30, pady=20)
+        self.frame.pack(fill="both", expand=True)
 
-        # Check if the output is non-empty
-        if result.stdout.strip():
-            print("Uncommitted changes detected.")
+        self.root.iconbitmap("branch_creator.ico")
+
+        self.label = tk.Label(self.frame, text="Enter SN number (if more than one, separated by comma): ")
+        self.label.pack()
+
+        self.entry = tk.Entry(self.frame)
+        self.entry.pack(fill="x")
+
+        self.label = tk.Label(self.frame, text="Enter feature description: ")
+        self.label.pack()
+
+        self.entry_feature = tk.Entry(self.frame)
+        self.entry_feature.pack(fill="x")
+
+        self.button = tk.Button(self.frame, text="Create branch name", command=self.process_input)
+        self.button.pack()
+
+        spacer = tk.Label(self.frame, text="", height=1)  # height=2 for vertical space (you can adjust this)
+        spacer.pack()
+
+        self.label = tk.Label(self.frame, text="branch name:")
+        self.label.pack()
+
+        self.entry_branch_name = tk.Entry(self.frame)
+        self.entry_branch_name.pack(fill="x")
+
+        self.btnFromClipBoard = tk.Button(self.frame, text="get from Clipboard", command=self.process_input_clipboard)
+        self.btnFromClipBoard.pack()
+
+        self.spacer = tk.Label(self.frame, text="", height=1)  # height=2 for vertical space (you can adjust this)
+        self.spacer.pack()
+
+        self.checkbox_var = tk.IntVar()
+        self.checkbox = tk.Checkbutton(self.root, text="Reset hard if there are uncommitted changes", variable=self.checkbox_var)
+        self.checkbox.pack()
+
+        self.btnFromClipBoard = tk.Button(self.frame, text="create branch", command=self.process_create_branch)
+        self.btnFromClipBoard.pack()
+
+        self.root.update()  # Force a layout update
+        self.root.geometry('')  # Tell Tkinter to auto-size based on content
+
+    def normalize_text(self,text):
+        text = unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("ASCII")
+        text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+        text = re.sub(r"\s+", "-", text)
+        return text
+
+    def check_no_uncommitted_changes(self):
+        try:
+            # Run 'git status --porcelain' to check for changes
+            result = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True)
+
+            # Check if the output is non-empty
+            if result.stdout.strip():
+                print("Uncommitted changes detected.")
+                return False
+            else:
+                print("No uncommitted changes.")
+                return True
+        except subprocess.CalledProcessError as e:
+            print(f"Error while running git status: {e}")
             return False
-        else:
-            print("No uncommitted changes.")
-            return True
-    except subprocess.CalledProcessError as e:
-        print(f"Error while running git status: {e}")
-        return False
-    
-def get_current_branch():
-    try:
-        # Run 'git rev-parse --abbrev-ref HEAD' to get the current branch
-        result = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], capture_output=True, text=True)
         
-        # Capture and return the branch name
-        branch_name = result.stdout.strip()
-        return branch_name
-    except subprocess.CalledProcessError as e:
-        print(f"Error while getting current branch: {e}")
-        return None    
+    def get_current_branch(self):
+        try:
+            # Run 'git rev-parse --abbrev-ref HEAD' to get the current branch
+            result = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], capture_output=True, text=True)
+            
+            # Capture and return the branch name
+            branch_name = result.stdout.strip()
+            return branch_name
+        except subprocess.CalledProcessError as e:
+            print(f"Error while getting current branch: {e}")
+            return None    
 
-def process_input_clipboard():
-    cliptext = pyperclip.paste()
-    splitted = cliptext.split("/")
-    entry.delete(0, tk.END)  # Clear the existing text
-    branchname = ''.join(splitted[1]).split("_")
-    entry.insert(0, branchname[0])
-    entry_feature.insert(0, branchname[2:])
-    entry_branch_name.insert(0, cliptext)
-    return
-
-def process_create_branch():
-    os.chdir(BETTING_FOLDER)
-    if check_no_uncommitted_changes():
-        branch = entry_branch_name.get()
-        reset = checkbox_var.get()
-        if reset:
-            os.system("git reset --hard")
-            os.system("git clean -f")
-        os.system(f"git checkout develop")
-        os.system(f"git pull")
-        os.system(f"git checkout -b {branch}")
-        current_branch = get_current_branch()
-        messagebox.showinfo("Input Processed", f'swithced to branch\n{current_branch}')
-    else:
-        current_branch = get_current_branch()
-        messagebox.showinfo("Input Processed", f'there are uncommitted changes in the current branch\n{current_branch}')
-    return
-
-def process_input():
-    danb = entry.get().split(",")
-
-    folder = ""
-    if danb[0].startswith("STRY"):
-        folder = "feature"
-    elif danb[0].startswith("BUGT"):
-        folder = "fix"
-    else:
-        folder = input("Enter FEATURE or FIX: ").lower()
-        validInput = False
-        for possibleAction in ["fix", "feature"]:
-            if folder == possibleAction:
-                validInput = True
-                break
-        if validInput == False:
-            print("Invalid command")
-            print()
-            print()
-            return
-
-    feature =  normalize_text(entry_feature.get()).lower()
-    
-    if (feature.strip() == ""):
-        messagebox.showinfo("missing data","please enter the feature name" )        
+    def process_input_clipboard(self):
+        cliptext = pyperclip.paste()
+        splitted = cliptext.split("/")
+        self.entry.delete(0, tk.END)  # Clear the existing text
+        branchname = ''.join(splitted[1]).split("_")
+        self.entry.insert(0, branchname[0])
+        self.entry_feature.insert(0, branchname[2:])
+        self.entry_branch_name.insert(0, cliptext)
         return
 
-    branch = f'{folder}/{"_".join(danb).replace(" ", "")}_BOZZONI_{feature}'
-    pyperclip.copy(branch)
+    def process_create_branch(self):
+        os.chdir(BETTING_FOLDER)
+        if self.check_no_uncommitted_changes():
+            branch = self.entry_branch_name.get()
+            reset = self.checkbox_var.get()
+            if reset:
+                os.system("git reset --hard")
+                os.system("git clean -f")
+            os.system(f"git checkout develop")
+            os.system(f"git pull")
+            os.system(f"git checkout -b {branch}")
+            current_branch = self.get_current_branch()
+            messagebox.showinfo("Input Processed", f'swithced to branch\n{current_branch}')
+        else:
+            current_branch = self.get_current_branch()
+            messagebox.showinfo("Input Processed", f'there are uncommitted changes in the current branch\n{current_branch}')
+        return
 
-    entry_branch_name.delete(0, tk.END)
-    entry_branch_name.insert(0, branch)
+    def process_input(self):
+        danb = self.entry.get().split(",")
 
-    print(f"Copied into clipboard: {branch}")
+        folder = ""
+        if danb[0].startswith("STRY"):
+            folder = "feature"
+        elif danb[0].startswith("BUGT"):
+            folder = "fix"
+        else:
+            folder = input("Enter FEATURE or FIX: ").lower()
+            validInput = False
+            for possibleAction in ["fix", "feature"]:
+                if folder == possibleAction:
+                    validInput = True
+                    break
+            if validInput == False:
+                print("Invalid command")
+                print()
+                print()
+                return
 
-app = tk.Tk()
-app.title("Sisal Git branch creator")
+        feature =  self.normalize_text(self.entry_feature.get()).lower()
+        
+        if (feature.strip() == ""):
+            messagebox.showinfo("missing data","please enter the feature name" )        
+            return
 
-frame = tk.Frame(app, padx=30, pady=20)
-frame.pack(fill="both", expand=True)
+        branch = f'{folder}/{"_".join(danb).replace(" ", "")}_{USERNAME}_{feature}'
+        pyperclip.copy(branch)
 
-app.iconbitmap("branch_creator.ico")
+        self.entry_branch_name.delete(0, tk.END)
+        self.entry_branch_name.insert(0, branch)
 
-label = tk.Label(frame, text="Enter SN number (if more than one, separated by comma): ")
-label.pack()
+        print(f"Copied into clipboard: {branch}")
 
-entry = tk.Entry(frame)
-entry.pack(fill="x")
+def main():    
+    root = tk.Tk()  #starts the GUI library
+    app = App(root) #create the object app and passes the root of the GUI
+    root.mainloop()
 
-label = tk.Label(frame, text="Enter feature description: ")
-label.pack()
-
-entry_feature = tk.Entry(frame)
-entry_feature.pack(fill="x")
-
-button = tk.Button(frame, text="Create branch name", command=process_input)
-button.pack()
-
-spacer = tk.Label(frame, text="", height=1)  # height=2 for vertical space (you can adjust this)
-spacer.pack()
-
-label = tk.Label(frame, text="branch name:")
-label.pack()
-
-entry_branch_name = tk.Entry(frame)
-entry_branch_name.pack(fill="x")
-
-btnFromClipBoard = tk.Button(frame, text="get from Clipboard", command=process_input_clipboard)
-btnFromClipBoard.pack()
-
-spacer = tk.Label(frame, text="", height=1)  # height=2 for vertical space (you can adjust this)
-spacer.pack()
-
-
-checkbox_var = tk.IntVar()
-checkbox = tk.Checkbutton(app, text="Reset hard if there are uncommitted changes", variable=checkbox_var)
-checkbox.pack()
-
-btnFromClipBoard = tk.Button(frame, text="create branch", command=process_create_branch)
-btnFromClipBoard.pack()
-
-app.update()  # Force a layout update
-app.geometry('')  # Tell Tkinter to auto-size based on content
-
-app.mainloop()
+if __name__ == "__main__":
+    main()
